d36ebd6d79c725b85bcc21728b7da1d67c81a9d2
Hammy Havoc <hammy@splitanatom.com>
2016-10-16 19:19:13 +0100

    First commit of app data.

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'studioblack-for-android',
+		wp_ws_url : 'https://www.studioblack.london/wp-appkit-api/studioblack-for-android',
+		wp_url : 'https://www.studioblack.london',
+		theme : 'q-android',
+		version : '1.0',
+		app_title : 'StudioBLACK',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : '%dB)NyYqo6wj6&y74u6qZOrV1qd@ux7vwWY@6Mj5Wh7btCEhjKMq4p4*)(xWjHep',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
